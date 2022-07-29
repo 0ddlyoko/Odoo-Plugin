c040ae2d0e90418fd27eefab0f34591df558c39e
0ddlyoko <0ddlyokoOfficial@gmail.com>
2022-07-29 09:34:41 +0200

    Add support to _inherit & _inherits

--- src/main/java/me/oddlyoko/odoo/models/reference/OdooModelReferenceContributor.java
+++ src/main/java/me/oddlyoko/odoo/models/reference/OdooModelReferenceContributor.java
@@ -9,15 +9,43 @@ import com.intellij.psi.PsiReferenceProvider;
 import com.intellij.psi.PsiReferenceRegistrar;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.python.PyTokenTypes;
+import com.jetbrains.python.psi.PyDictLiteralExpression;
+import com.jetbrains.python.psi.PyListLiteralExpression;
+import com.jetbrains.python.psi.PySetLiteralExpression;
 import com.jetbrains.python.psi.PyStringLiteralExpression;
 import com.jetbrains.python.psi.PyTargetExpression;
 import org.jetbrains.annotations.NotNull;
 
 public class OdooModelReferenceContributor extends PsiReferenceContributor {
+    public static final PsiElementPattern.Capture<? extends PsiElement> NAME_PATTERN =
+            PlatformPatterns.psiElement(PyStringLiteralExpression.class).afterSiblingSkipping(
+                    PlatformPatterns.psiElement().withElementType(PyTokenTypes.EQ),
+                    PlatformPatterns.psiElement(PyTargetExpression.class).withName("_name")
+            );
     public static final PsiElementPattern.Capture<? extends PsiElement> INHERIT_PATTERN =
             PlatformPatterns.psiElement(PyStringLiteralExpression.class).afterSiblingSkipping(
                     PlatformPatterns.psiElement().withElementType(PyTokenTypes.EQ),
                     PlatformPatterns.psiElement(PyTargetExpression.class).withName("_inherit")
+            );
+    public static final PsiElementPattern.Capture<? extends PsiElement> INHERIT_DICT_PATTERN =
+            PlatformPatterns.psiElement(PyStringLiteralExpression.class).withParent(
+                    PlatformPatterns.psiElement(PyListLiteralExpression.class).afterSiblingSkipping(
+                            PlatformPatterns.psiElement().withElementType(PyTokenTypes.EQ),
+                            PlatformPatterns.psiElement(PyTargetExpression.class).withName("_inherit")
+                    )
+            );
+    public static final PsiElementPattern.Capture<? extends PsiElement> INHERITS_PATTERN =
+            PlatformPatterns.psiElement(PyStringLiteralExpression.class).withAncestor(2,
+                    PlatformPatterns.or(
+                            PlatformPatterns.psiElement(PyDictLiteralExpression.class).afterSiblingSkipping(
+                                    PlatformPatterns.psiElement().withElementType(PyTokenTypes.EQ),
+                                    PlatformPatterns.psiElement(PyTargetExpression.class).withName("_inherits")
+                            ),
+                            PlatformPatterns.psiElement(PySetLiteralExpression.class).afterSiblingSkipping(
+                                    PlatformPatterns.psiElement().withElementType(PyTokenTypes.EQ),
+                                    PlatformPatterns.psiElement(PyTargetExpression.class).withName("_inherits")
+                            )
+                    )
             );
 
     @Override
@@ -29,6 +57,9 @@ public class OdooModelReferenceContributor extends PsiReferenceContributor {
                 return new PsiReference[] { new OdooModelReference(element) };
             }
         };
+        registrar.registerReferenceProvider(NAME_PATTERN, provider);
         registrar.registerReferenceProvider(INHERIT_PATTERN, provider);
+        registrar.registerReferenceProvider(INHERIT_DICT_PATTERN, provider);
+        registrar.registerReferenceProvider(INHERITS_PATTERN, provider);
     }
 }
